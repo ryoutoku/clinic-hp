4d39a6253abbb4fcbbb2b4d9e1b097930a90591f
Akinori Hayashi <invisiblechain@hotmail.co.jp>
2019-07-17 21:31:14 +0900

    feat: お知らせページ追加

---
+++ src/views/Information.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="base-view">
+    <h1 class="title is-3">お知らせ</h1>
+    <div class="columns is-centered">
+      <TextCard>
+        <template v-slot:title>2019年9月17日(火)は、休診となります。</template>
+        <template v-slot:content>
+          <img src="@/assets/Capture.jpg" alt="9月の休診予定" />
+        </template>
+      </TextCard>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TextCard from '@/components/TextCard.vue';
+import BaseView from '@/views/BaseView.vue';
+
+@Component({
+  components: {
+    TextCard,
+    BaseView
+  }
+})
+export default class Guid extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.base-view {
+  margin: 1rem;
+  width: 85%;
+  display: inline-block;
+  text-align: center;
+}
+</style>
