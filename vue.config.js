8bbab568abc41bb7702b9b8eef4d0a7be8baefd8
ryoutoku <invisible060@gmail.com>
2023-01-04 22:37:17 +0900

    fix: change module see: https://github.com/chrisvfritz/prerender-spa-plugin/issues/414

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require('path');
+const PrerenderSPAPlugin = require('@dreysolano/prerender-spa-plugin');
 
 module.exports = {
   productionSourceMap: true,
   outputDir: 'docs',
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     // プロダクトモードでのみ追加
     if (process.env.NODE_ENV === 'production') {
       return {
@@ -16,30 +16,30 @@ module.exports = {
             // 生成したいページ
             routes: ['/', '/access', '/care', '/doctor', '/guide', '/pageNotFound'],
 
-            postProcess (renderedRoute) {
-              if (renderedRoute.originalRoute === "/pageNotFound"){
+            postProcess(renderedRoute) {
+              if (renderedRoute.originalRoute === '/pageNotFound') {
                 // pageNotFoundの場合は404.htmlを生成する
-                renderedRoute.route = "/";
-                renderedRoute.outputPath = path.join(this.staticDir, "404.html")
-              }else{
+                renderedRoute.route = '/';
+                renderedRoute.outputPath = path.join(this.staticDir, '404.html');
+              } else {
                 // Ignore any redirects.
-                renderedRoute.route = renderedRoute.originalRoute
+                renderedRoute.route = renderedRoute.originalRoute;
               }
 
               // Basic whitespace removal. (Don't use this in production.)
-              renderedRoute.html = renderedRoute.html.split(/>[\s]+</gmi).join('><')
+              renderedRoute.html = renderedRoute.html.split(/>[\s]+</gim).join('><');
 
               // Remove /index.html from the output path if the dir name ends with a .html file extension.
               // For example: /dist/dir/special.html/index.html -> /dist/dir/special.html
               if (renderedRoute.route.endsWith('.html')) {
-                renderedRoute.outputPath = path.join(__dirname, 'dist', renderedRoute.route)
+                renderedRoute.outputPath = path.join(__dirname, 'dist', renderedRoute.route);
               }
-      
-              return renderedRoute
+
+              return renderedRoute;
             },
-          })
-        ]
-      }
+          }),
+        ],
+      };
     }
-  }
-}
+  },
+};
